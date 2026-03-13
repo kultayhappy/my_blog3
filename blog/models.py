ba346db974c25dedc34e2457840c36ff0b79c47d
freemonic <luckmusk@yahoo.com>
2026-03-13 11:53:28 +0500

    Add: Comment model with post and author foreign keys

--- blog/models.py
+++ blog/models.py
@@ -47,3 +47,38 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.pk})
+    
+    
+class Comment(models.Model):
+    """Модель комментария к посту."""
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пост'
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+
+    text = models.TextField(
+        verbose_name='Текст комментария'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
