c2b9251d4f878516139f035b4a9f46f24b37213f
freemonic <luckmusk@yahoo.com>
2026-03-14 01:50:26 +0500

    Add: Integration tests for Post views (list, detail, create)

--- blog/tests.py
+++ blog/tests.py
@@ -61,3 +61,48 @@ class CommentModelTest(TestCase):
             text='Второй комментарий'
         )
         self.assertEqual(self.post.comments.count(), 2)
+        
+        
+class PostViewTest(TestCase):
+    """Integration-тесты для views."""
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='viewuser',
+            password='pass123'
+        )
+        self.post = Post.objects.create(
+            title='Тестовый пост',
+            content='Содержимое',
+            author=self.user,
+            published=True
+        )
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('blog:post_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Тестовый пост')
+
+    def test_post_detail_view(self):
+        response = self.client.get(
+            reverse('blog:post_detail', kwargs={'pk': self.post.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_create_view_authenticated(self):
+        self.client.login(username='viewuser', password='pass123')
+        post_data = {
+            'title': 'Новый пост',
+            'content': 'Содержимое',
+            'published': True,
+        }
+        response = self.client.post(
+            reverse('blog:post_create'),
+            data=post_data,
+            follow=True
+        )
+        self.assertEqual(Post.objects.filter(title='Новый пост').count(), 1)
+
+    def test_post_create_view_not_authenticated(self):
+        response = self.client.get(reverse('blog:post_create'))
+        self.assertEqual(response.status_code, 302)
