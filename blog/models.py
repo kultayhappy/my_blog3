043521a48a736b4b06de529f3eafde42a1e7d0d9
freemonic <luckmusk@yahoo.com>
2026-03-13 11:34:21 +0500

    Add: Post model with title, content, author, timestamps

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    """Модель поста в блоге."""
+
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Заголовок'
+    )
+
+    content = models.TextField(
+        verbose_name='Содержимое'
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата обновления'
+    )
+
+    published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
