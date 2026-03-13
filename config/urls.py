f2ca7d622cda85731ecad8a8564817fda793a5ca
freemonic <luckmusk@yahoo.com>
2026-03-14 01:30:17 +0500

    Add: Login and logout URL patterns for authentication

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')), 
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
