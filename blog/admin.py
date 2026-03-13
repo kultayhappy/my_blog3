7b37a5a4eb681527e0eae39e8506c0165c4f6a4b
freemonic <luckmusk@yahoo.com>
2026-03-13 12:34:03 +0500

    Add: Register Post and Comment models in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at', 'published']
+    list_filter = ['published', 'created_at', 'author']
+    search_fields = ['title', 'content']
+    date_hierarchy = 'created_at'
+    list_editable = ['published']
+    list_per_page = 20
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['text_short', 'author', 'post', 'created_at']
+    list_filter = ['author', 'post', 'created_at']
+    search_fields = ['text']
+
+    def text_short(self, obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+
+    text_short.short_description = 'Текст'
+
